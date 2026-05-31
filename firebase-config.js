9eb75e75dd720d0141f4a41566afa1873f0ba179
shubhamkilania <shubhamkilania67@gmail.com>
2026-05-31 21:15:43 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,24 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAxUg2SvTSXMacfSwNnBfQPRRyq1nTvM6c",
+    authDomain: "cointube-1411.firebaseapp.com",
+    databaseURL: "https://cointube-1411-default-rtdb.firebaseio.com",
+    projectId: "cointube-1411",
+    storageBucket: "cointube-1411.firebasestorage.app",
+    messagingSenderId: "539469111589",
+    appId: "1:539469111589:web:88d18bf7eeda371f27bacc",
+    measurementId: "G-DLW9DDEKM0"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
