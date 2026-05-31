7f5f5710e8d82db486156c98c7fa6cb1982f1a79
shubhamkilania <shubhamkilania67@gmail.com>
2026-05-31 20:01:26 +0530

    Implement referral code generation feature
    Added referral code generation and handling for new users.

--- app.js
+++ app.js
@@ -4,3 +4,27 @@ function toggleMenu(){
     document.getElementById("sidebar")
     .classList.toggle("active");
 }
+referredBy = "SHUBH1411";
+
+function generateReferralCode(name){
+
+    let firstPart = name
+        .substring(0,4)
+        .toUpperCase();
+
+    let randomPart = Math.floor(
+        1000 + Math.random() * 9000
+    );
+
+    return firstPart + randomPart;
+}
+
+const OWNER_REFERRAL = "SHUBH1411";
+
+if(referralInput === "SHUBH1411"){
+
+    newUserCoins += 100;
+
+    ownerCoins += 50;
+
+}
