0bb28d99fba1314f35ffdce84b51804869641318
shubhamkilania <shubhamkilania67@gmail.com>
2026-06-01 13:12:32 +0530

    Downgrade Firebase SDK version in config

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
 
 import {
   getAuth,
   RecaptchaVerifier,
   signInWithPhoneNumber
-} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
 import {
   getFirestore,
   doc,
   setDoc,
   getDoc
-} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "PASTE_YOUR_REAL_API_KEY_HERE",
@@ -22,12 +22,12 @@ const firebaseConfig = {
   appId: "1:539469111589:web:88d18bf7eeda371f27bacc"
 };
 
+// Init Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
-
 const db = getFirestore(app);
 
+// ✅ IMPORTANT: export auth also
 export {
   app,
   auth,
@@ -36,5 +36,7 @@ export {
   signInWithPhoneNumber,
   doc,
   setDoc,
+  getDoc
+};
   getDoc
 };
