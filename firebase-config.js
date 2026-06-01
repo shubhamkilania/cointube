cfbe88380579a3159a2d91f66c1d9c1aa0c87b82
shubhamkilania <shubhamkilania67@gmail.com>
2026-06-01 14:22:33 +0530

    Upgrade Firebase SDK and update apiKey
    Updated Firebase SDK imports to version 12.14.0 and modified apiKey.

--- firebase-config.js
+++ firebase-config.js
@@ -1,36 +1,39 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
 
 import {
   getAuth,
   RecaptchaVerifier,
   signInWithPhoneNumber
-} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
 
 import {
   getFirestore,
   doc,
   setDoc,
   getDoc
-} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_REAL_API_KEY_HERE",
+  apiKey: "AIzaSyAxUg2SvTSXMacfSwNnBfQPRRyq1nTvM6c",
   authDomain: "cointube-1411.firebaseapp.com",
+  databaseURL: "https://cointube-1411-default-rtdb.firebaseio.com",
   projectId: "cointube-1411",
   storageBucket: "cointube-1411.firebasestorage.app",
   messagingSenderId: "539469111589",
-  appId: "1:539469111589:web:88d18bf7eeda371f27bacc"
+  appId: "1:539469111589:web:88d18bf7eeda371f27bacc",
+  measurementId: "G-DLW9DDEKM0"
 };
 
-// INIT APP
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// SERVICES
+// Services
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// EXPORTS
+// Export everything needed
 export {
+  app,
   auth,
   db,
   RecaptchaVerifier,
