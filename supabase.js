498fd0cad4bf2bf17a9a6b9cca386336bfb491ff
shubhamkilania <shubhamkilania67@gmail.com>
2026-06-04 15:03:56 +0530

    Update Supabase key in supabase.js

--- supabase.js
+++ supabase.js
@@ -4,7 +4,7 @@ from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 const supabaseUrl =
 "https://mpjtrrothusmeyukmdyk.supabase.co";
 
-const supabaseKey = "sb_publishable_2km10..."
+const supabaseKey = "sb_publishable_2km10GTUstXnzEEijhNN5A_5egc5wRO"
 
 export const supabase =
 createClient(
